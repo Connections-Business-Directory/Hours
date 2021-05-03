0dbf713976358dc6d654b610d4111c160b047cfe
Steven A. Zahm <shazahm1@hotmail.com>
2021-05-03 13:58:40 -0400

    Correct code alignment.

--- connections_hours.php
+++ connections_hours.php
@@ -844,21 +844,22 @@ if ( ! class_exists('Connections_Business_Hours') ) {
 	 */
 	function Connections_Business_Hours() {
 
-			if ( class_exists('connectionsLoad') ) {
+		if ( class_exists( 'connectionsLoad' ) ) {
 
-					return Connections_Business_Hours::instance();
+			return Connections_Business_Hours::instance();
 
-			} else {
+		} else {
 
-					add_action(
-							'admin_notices',
-							function() {
-								echo '<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Business Hours.</p></div>';
-							}
-					);
+			add_action(
+				'admin_notices',
+				function() {
 
-					return FALSE;
-			}
+					echo '<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Business Hours.</p></div>';
+				}
+			);
+
+			return false;
+		}
 	}
 
 	/**
